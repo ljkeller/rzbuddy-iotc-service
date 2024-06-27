f14e00be5d1d5f402d1c9feb66885f55d2de25f2
ljkeller <kellerjlucas@gmail.com>
2024-06-27 17:59:08 -0600

    WIP IOT connect management
    Modifying an existing template I've previously built.

---
+++ manage_iotconnect.py
@@ -0,0 +1,228 @@
+"""SPARK & IoTConnect Integration"""
+
+import json
+import socket
+import sys
+import signal
+import time
+import random
+from datetime import datetime
+from typing import Dict, Any, List
+
+from iotconnect import IoTConnectSDK
+
+class SignalException(Exception):
+    """Custom exception to exit gracefully"""
+
+class IoTConnectClient:
+    """Send SPARK data to IoTConnect platform using IoTConnect SDK."""
+    def __init__(self, config_path: List[str]) -> None:
+        self.config = {}
+        self.load_config(config_path)
+        print(f"Configuration loaded: {self.config}")
+        # TODO: Put more of these options in the configuration file
+        self.sdk_options = {
+            "certificate": {
+                "SSLKeyPath": self.config['ssl']['keyPath'],
+                "SSLCertPath": self.config['ssl']['certPath'],
+                "SSLCaPath": self.config['ssl']['caPath'],
+            },
+            "offlineStorage": {
+                "disabled": False,
+                "availSpaceInMb": 0.01,
+                "fileCount": 5,
+                "keepalive": 60
+            },
+            "skipValidation": False,
+            "discoveryUrl": self.config['networking']['discoveryUrl'],
+            "IsDebug": True,
+            "transmit_interval_seconds": 2
+        }
+        self.sdk = None
+        self.device_list = []
+        self.setup_exit_handler()
+
+        self.run_continuously = True
+        self.last_payload_str = None
+        self.next_transmit_time = time.time()
+        print("IoTConnect service initialized")
+
+    def load_config(self, config_paths: List[str]) -> None:
+        """Dependency inject client configuration"""
+        try:
+            for config_path in config_paths:
+                with open(config_path, encoding="utf-8") as f:
+                    # Load configuration from JSON file(s) with UNIQUE KEYS
+                    self.config.update(json.load(f))
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+
+    def get_spark_datagram_socket(self) -> socket.socket:
+        """Attempt connection (continuously) to SPARK producer socket"""
+        max_retry_backoff_s = 8
+        retry_backoff_s = 1
+        sock = None
+        while self.run_continuously:
+            try:
+                for addr in socket.getaddrinfo(self.config['spark_socket_ipv6'], self.config['spark_socket_port'], socket.AF_INET6, socket.SOCK_DGRAM, 0, socket.AI_PASSIVE):
+                    af, socktype, proto, _, sa = addr
+                    try:
+                        sock = socket.socket(af, socktype, proto)
+                        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    except OSError as msg:
+                        print(f"Socket init failed: {msg}. Retrying...")
+                        sock = None
+                        continue
+                    try:
+                        sock.bind(sa)
+                    except OSError as msg:
+                        print(f"Socket bind failed: {msg}. Retrying...")
+                        sock.close()
+                        sock = None
+                        continue
+                    # Success
+                    break
+
+                if sock is None:
+                    raise ConnectionError("Failed to create socket")
+
+                return sock
+            except SignalException:
+                sys.exit(0)
+            except Exception as msg:
+                retry_backoff_s = min(max_retry_backoff_s, retry_backoff_s * 2)
+                time.sleep(retry_backoff_s)
+
+
+    def setup_exit_handler(self) -> None:
+        """Define exit conditions as application will typically run indefinitely"""
+        signal.signal(signal.SIGINT, self.exit_handler)
+        signal.signal(signal.SIGTERM, self.exit_handler)
+
+    def exit_handler(self, _sig, _frame) -> None:
+        """Stop IoT/SPARK integration"""
+        print("Exit signal detected. Exiting...")
+        self.run_continuously = False
+        raise SignalException("Exit signal detected")
+    
+    def receive_json_payload(self, sock: socket.socket) -> Dict[str, Any]:
+        """Receive JSON payload from producer socket"""
+        if sock is None:
+            raise ConnectionError("SPARK consumer socket not connected")
+        
+        bytes_data, _addr = sock.recvfrom(1024)
+        if not bytes_data:
+            raise ConnectionError("SPARK producer socket closing")
+
+        try:
+            return json.loads(bytes_data.decode('utf-8'))
+        except Exception as e:
+            print(f"Failed to parse JSON payload: {e}")
+            return dict()
+
+    def device_callback(self, msg: Dict[str, Any]) -> None:
+        print("\n--- Device Callback ---")
+        print(json.dumps(msg))
+        cmd_type = None
+        if msg is not None and len(msg.items()) != 0:
+            cmd_type = msg["ct"] if "ct" in msg else None
+        # Other Command
+        if cmd_type == 0:
+            """
+            * Type    : Public Method "sendAck()"
+            * Usage   : Send device command received acknowledgment to cloud
+            * 
+            * - status Type
+            *     st = 6; // Device command Ack status 
+            *     st = 4; // Failed Ack
+            * - Message Type
+            *     msgType = 5; // for "0x01" device command 
+            """
+            data=msg
+            if data is None:
+                #print(data)
+                if "id" in data:
+                    if "ack" in data and data["ack"]:
+                        self.sdk.sendAckCmd(data["ack"],7,"sucessfull",data["id"])  #fail=4,executed= 5,sucess=7,6=executedack
+                else:
+                    if "ack" in data and data["ack"]:
+                        self.sdk.sendAckCmd(data["ack"],7,"sucessfull") #fail=4,executed= 5,sucess=7,6=executedack
+        else:
+            print("rule command",msg)
+        
+
+    def device_firmware_callback(self, msg: Dict[str, Any]) -> None:
+        print("\n--- Firmware Command Message Received ---")
+        print("--- No further business logic implemented ---")
+        print(json.dumps(msg))
+
+    def device_connection_callback(self, msg: Dict[str, Any]) -> None:
+        print("\n--- Device coonnection callback: no further business logic implemented ---")
+        print(json.dumps(msg))
+
+    def twin_update_callback(self, msg: Dict[str, Any]) -> None:
+        print("--- Twin Message Received ---")
+        print("--- No further business logic implemented ---")
+        print(json.dumps(msg))
+
+    def send_json_payload_throttled(self, feed_data: Dict[str, Any]) -> bool:
+        if feed_data is None or len(feed_data.items()) == 0:
+            return False
+
+        try:
+            # Snapshot payload before altering it
+            cur_payload_str = json.dumps(feed_data)
+            # Maybe we want to send the same data multiple times if lots of time has passed
+            if cur_payload_str == self.last_payload_str or time.time() < self.next_transmit_time:
+                return False
+
+            lat_min, lat_max = 39.0, 40.0
+            long_min, long_max = -105.0, -104.0
+            feed_data['location'] = [round(random.uniform(lat_min, lat_max), 5), round(random.uniform(long_min, long_max), 5)]
+            payload = [{
+                "uniqueId": self.config['ids']['uniqueId'],
+                "time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000Z"),
+                "data": feed_data
+            }]
+            print(f"Sending payload: {payload}")
+            self.sdk.SendData(payload)
+
+            self.last_payload_str = cur_payload_str
+            self.next_transmit_time = time.time() + self.sdk_options['transmit_interval_seconds']
+            return True
+        except Exception as e:
+            print(f"Caught exception {e} while trying to send JSON payload to IoT connect.")
+            return False
+
+    def run_telemetry_continuously(self) -> None:
+        """Run IoTConnect client continuously"""
+        max_retry_backoff_s = 300
+        retry_backoff_s = 5
+        while self.run_continuously:
+            try:
+                print("Starting IoTConnect SDK")
+                with IoTConnectSDK(self.config['ids']['uniqueId'], self.config['ids']['sid'], self.sdk_options, self.device_connection_callback) as self.sdk:
+                    self.device_list = self.sdk.Getdevice()
+                    self.sdk.onDeviceCommand(self.device_callback)
+                    self.sdk.onTwinChangeCommand(self.twin_update_callback)
+                    self.sdk.onOTACommand(self.device_firmware_callback)
+                    self.sdk.onDeviceChangeCommand(self.device_connection_callback)
+                    self.sdk.getTwins()
+                    self.device_list = self.sdk.Getdevice()
+                    spark_socket = self.get_spark_datagram_socket()
+                    print("Forwarding telemetry data to IoTConnect when it arrives...")
+                    while True:
+                        payload = self.receive_json_payload(spark_socket)
+                        self.send_json_payload_throttled(payload)
+            except SignalException:
+                sys.exit(0)
+            # exponential backoff
+            except Exception as ex:
+                print(f'Backing off from {ex}')
+                time.sleep(retry_backoff_s)
+                retry_backoff_s = min(max_retry_backoff_s, retry_backoff_s * 2)
+
+if __name__ == "__main__":
+    client = IoTConnectClient(['/opt/rzbuddy/iot/secrets.json', '/opt/rzbuddy/iot/config.json'])
+    client.run_telemetry_continuously()
